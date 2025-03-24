43011b842b4c5b9a0dadc8889aaedc912939b940
new1333 <dearest@abouttime.cn>
2025-03-24 10:41:58 +0800

    https

--- stores/useWeatherStore.js
+++ stores/useWeatherStore.js
@@ -3,7 +3,7 @@ import { toast } from "vue3-toastify";
 
 const weatherApiKey = "3978da9f7417a95247e8f31df0cf1f27";
 const weatherLocation = "Basel";
-const weatherApiBaselUrl = `http://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&appid=${weatherApiKey}&units=metric`;
+const weatherApiBaselUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&appid=${weatherApiKey}&units=metric`;
 
 export const useWeatherStore = defineStore("weatherStore", {
   state: () => {
