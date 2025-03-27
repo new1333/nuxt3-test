82d80adee841a6db45721672149add30241514ad
new1333 <dearest@abouttime.cn>
2025-03-27 09:04:48 +0800

    refactor: weather constants

---
+++ constants/weather.ts
@@ -0,0 +1,4 @@
+const weatherApiKey = "3978da9f7417a95247e8f31df0cf1f27";
+const weatherLocation = "Basel";
+
+export const weatherApiBaselUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&appid=${weatherApiKey}&units=metric`;
--- server/api/getWeather.ts
+++ server/api/getWeather.ts
@@ -1,8 +1,5 @@
+import { weatherApiBaselUrl } from "~/constants/weather";
 import type { WeatherDataResponse } from "~/types/weather/Weather";
-
-const weatherApiKey = "3978da9f7417a95247e8f31df0cf1f27";
-const weatherLocation = "Basel";
-const weatherApiBaselUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&appid=${weatherApiKey}&units=metric`;
 
 export default defineEventHandler(() => {
   return $fetch<WeatherDataResponse>(weatherApiBaselUrl);
